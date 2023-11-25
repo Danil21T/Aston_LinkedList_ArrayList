8ba32e56135de7e711cc2ddf59a16113ef221329
Данил Токарев <danil2002tokarev@mail.ru>
2023-11-26 00:18:16 +0300

    Homework is done!

--- src/main/java/org/example/MyArrayList.java
+++ src/main/java/org/example/MyArrayList.java
@@ -1,4 +1,208 @@
 package org.example;
 
-public class MyArrayList {
+import java.util.Arrays;
+
+/**
+ * Represents a resizable array list.
+ *
+ * @param <T> the type of elements in the list
+ */
+public class MyArrayList<T> {
+
+    private final int DEFAULT_SIZE = 16;
+    private Object[] mas;
+    private int point = 0;
+    private int size;
+
+    /**
+     * Constructs an empty MyArrayList with the default initial capacity of 16.
+     */
+    public MyArrayList() {
+        this.size = DEFAULT_SIZE;
+        initDefaultMassive();
+    }
+
+    /**
+     * Constructs an empty MyArrayList with the specified initial capacity.
+     *
+     * @param size the initial capacity
+     */
+    public MyArrayList(int size) {
+        this.size = size;
+        initDefaultMassive();
+    }
+
+    /**
+     * Constructs a new MyArrayList that contains the elements of the specified MyArrayList.
+     *й
+     * @param copyFrom the MyArrayList to copy elements from
+     */
+    public MyArrayList(MyArrayList<T> copyFrom){
+        this(copyFrom.size);
+        System.arraycopy(copyFrom.mas, 0, this.mas, 0, size);
+    }
+
+    /**
+     * Returns the element at the specified index in this list.
+     *
+     * @param index the index of the element to return
+     * @return the element at the specified index, or null if the index is out of range
+     */
+    public Object get(int index) {
+        if (index >= 0 && index < point) {
+            return mas[index];
+        } else return null;
+    }
+
+    /**
+     * Appends the specified element to the end of this list.
+     *
+     * @param item the element to be added
+     */
+    public void add(T item) {
+        if (point == size - 1)
+            resizeMassive();
+        mas[point++] = item;
+    }
+
+    /**
+     * Inserts the specified element at the specified position in this list.
+     *
+     * @param item the element to be added
+     * @param index  the index at which to add the element
+     */
+    public void add(T item, int index) {
+        if (index >= 0 && index < point) {
+            if (point == size - 1)
+                resizeMassive();
+            int end_ind = index;
+            while (mas[end_ind] != null) {
+                end_ind++;
+            }
+            for (int j = end_ind; j > index; j--) {
+                mas[j] = mas[j - 1];
+            }
+            mas[index] = item;
+            point++;
+        } else {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    /**
+     * Removes the element at the specified index in this list.
+     *
+     * @param index the index of the element to be removed
+     */
+    public void remove(int index) {
+        if (index >= 0 && index < point) {
+            for (int i = index; i < size - 1; i++) {
+                mas[i] = mas[i + 1];
+            }
+            mas[point - 1] = null;
+            point--;
+        }
+        else{
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    /**
+     * Removes the first occurrence of the specified element from this list.
+     *
+     * @param o the element to be removed
+     * @return true if the element is successfully removed, false otherwise
+     */
+    public boolean remove(Object o) {
+        int index = 0;
+        for (Object elem : mas) {
+            if(elem == null)
+                break;
+            if (elem.equals(o)) {
+                remove(index);
+                return true;
+            }
+            index++;
+        }
+        return false;
+    }
+
+    /**
+     * Removes all of the elements from this list.
+     */
+    public void clear() {
+        Arrays.fill(mas, null);
+        point = 0;
+    }
+
+    /**
+     * Sorts the elements in this list in ascending order.
+     */
+    public void sort(){
+        if(point <= 1)
+            return;
+        for (int out = point - 1; out >= 1; out--){
+            for (int in = 0; in < out; in++){
+                if(compare(in, in + 1) > 0){
+                    swap(in, in+1);
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns a string representation of this list.
+     *
+     * @return a string representation of this list
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(Object o : mas){
+            if(o == null) break;
+            sb.append(o).append(" ");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Swapping array elements.
+     *
+     */
+    private void swap(int index1, int index2){
+        Object mem = mas[index1];
+        mas[index1] = mas[index2];
+        mas[index2] = mem;
+    }
+
+    /**
+     * Comparison of two elements.
+     *
+     * @return less than 0 - if the 2nd is greater than the 1st, zero - if they are equal, more than 0 - if the 1st is greater than 2-uj
+     */
+    private int compare(int index1, int index2){
+        return Integer.compare(mas[index1].hashCode(), mas[index2].hashCode());
+    }
+
+    /**
+     * Resize of the array.
+     *
+     */
+    private void resizeMassive() {
+        int old_size = size;
+        size = size + DEFAULT_SIZE >> 1;
+        Object[] copy = new Object[size];
+        System.arraycopy(mas, 0, copy, 0, old_size);
+        mas = copy;
+    }
+
+    /**
+     * Initializing the initial array and filling with null values.
+     *
+     */
+    private void initDefaultMassive() {
+        mas = new Object[size];
+        Arrays.fill(mas, null);
+    }
+
 }
--- src/main/java/org/example/MyLinkedList.java
+++ src/main/java/org/example/MyLinkedList.java
@@ -1,4 +1,247 @@
 package org.example;
 
-public class MyLinkedList {
+/**
+ * A class that represents a linked list.
+ *
+ * @param <t> the type of elements in the linked list
+ */
+public class MyLinkedList<t> {
+    private Node<t> head;
+    private Node<t> tail;
+    private int size;
+
+    /**
+     * A private inner class that represents a node in the linked list.
+     *
+     * @param <t> the type of element in the node
+     */
+    private static class Node<t> {
+        private t data;
+        private Node<t> previous;
+        private Node<t> next;
+
+        /**
+         * Constructs a node with the specified data.
+         *
+         * @param data the data for the node
+         */
+        public Node(t data) {
+            this.data = data;
+        }
+    }
+
+    /**
+     * Constructs an empty linked list.
+     */
+    public MyLinkedList(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    /**
+     * Constructs a new linked list by copying another linked list.
+     *
+     * @param copyFrom the linked list to copy from
+     */
+    public MyLinkedList(MyLinkedList copyFrom){
+        head = copyFrom.head;
+        tail = copyFrom.tail;
+        size = copyFrom.size;
+    }
+
+    /**
+     * Adds an element to the end of the linked list.
+     *
+     * @param item the element to add
+     */
+    public void add(t item) {
+        Node<t> newNode = new Node<>(item);
+
+        if(head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.previous = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        size++;
+    }
+
+    /**
+     * Adds an element at a specified index in the linked list.
+     *
+     * @param item the element to add
+     * @param index the index at which to add the element
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    public void add(t item, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        if (index == size) {
+            add(item);
+            return;
+        }
+
+        Node<t> newNode = new Node<>(item);
+
+        if (index == 0) {
+            newNode.next = head;
+            head.previous = newNode;
+            head = newNode;
+        } else {
+            Node<t> current = getNode(index);
+            current.previous.next = newNode;
+            newNode.previous = current.previous;
+            newNode.next = current;
+            current.previous = newNode;
+        }
+
+        size++;
+    }
+
+    /**
+     * Removes an element from the linked list.
+     *
+     * @param item the element to remove
+     */
+    public void remove(t item) {
+        Node<t> current = head;
+        while (current != null) {
+            if (current.data.equals(item)) {
+                if (current == head) {
+                    head = current.next;
+                } else if (current == tail) {
+                    tail = current.previous;
+                } else {
+                    current.previous.next = current.next;
+                    current.next.previous = current.previous;
+                }
+
+                size--;
+                return;
+            }
+
+            current = current.next;
+        }
+    }
+
+    /**
+     * Removes an element at a specified index in the linked list.
+     *
+     * @param index the index of the element to remove
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        if (index == 0) {
+            head = head.next;
+        } else if (index == size - 1) {
+            tail = tail.previous;
+        } else {
+            Node<t> current = getNode(index);
+            current.previous.next = current.next;
+            current.next.previous = current.previous;
+        }
+
+        size--;
+    }
+
+    /**
+     * Clears the linked list, removing all elements.
+     */
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    /**
+     * Sorts the linked list using bubble sort.
+     */
+    public void sort() {
+        if (size <= 1) {
+            return;
+        }
+        boolean swapped;
+        do {
+            swapped = false;
+            Node<t> current = head;
+            while (current.next != null) {
+                if (compare(current.data, current.next.data) > 0) {
+                    t temp = current.data;
+                    current.data = current.next.data;
+                    current.next.data = temp;
+                    swapped = true;
+                }
+                current = current.next;
+            }
+        } while (swapped);
+
+    }
+
+    /**
+     * Gets the element at a specified index in the linked list.
+     *
+     * @param index the index of the element to get
+     * @return the element at the specified index
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    public t get(int index){
+        return getNode(index).data;
+    }
+
+    /**
+     * Returns a string representation of the linked list.
+     *
+     * @return a string representation of the linked list
+     */
+    public String toString() {
+        Node<t> current = head;
+        StringBuilder sb = new StringBuilder();
+        while (current != null) {
+            sb.append(current.data).append(" ");
+            current = current.next;
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Compares two elements for sorting purposes.
+     *
+     * @param elem1 the first element to compare
+     * @param elem2 the second element to compare
+     * @return a negative integer, zero, or a positive integer as the first element
+     *         is less than, equal to, or greater than the second element
+     */
+    private int compare(t elem1, t elem2) {
+        return Integer.compare(elem1.hashCode(), elem2.hashCode());
+    }
+
+    /**
+     * Gets the node at a specified index in the linked list.
+     *
+     * @param index the index of the node to get
+     * @return the node at the specified index
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    private Node<t> getNode(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        Node<t> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        return current;
+    }
 }
